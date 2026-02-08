0ea12716644ed644135bddde494fa6579ca4f3e5
Shakela <shakela.hossain@gmail.com>
2026-02-07 19:51:30 -0500

    Add color tokens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './tokens/colors.css'  
+import './tokens/themes/light.css' 
+import './tokens/themes/dark.css' 
+
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.documentElement.dataset.theme = "dark";
+  }, []);
 
   return (
     <>
@@ -18,7 +25,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button style={{ color: 'var(--color-primary)' }} onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
